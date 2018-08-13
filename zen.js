511a9ba3437e2c968dfa9669714dc69e68228bb1
OsoPolardeFuego <jvkiet@gmail.com>
2018-08-13 14:30:29 -0400

    first commit

---
+++ zen.js
@@ -0,0 +1,31 @@
+console.log('test');
+var today = new Date();
+today = today.toISOString().slice(0,10)
+
+
+var zendesk = require('node-zendesk');
+
+var client = zendesk.createClient({
+  username:  'justin.vuong@consensys.net',
+  token:     'h5GZTXGot6OlA8Nw4LbwAHuiBxLcbEiB5xIBKB1H',
+  remoteUri: 'https://consensys.zendesk.com/api/v2'
+});
+
+
+	//var query = "created:"+today+" type:ticket"
+	var query = 5691
+	client.search.query(query, function (err, req, result) {
+		result.forEach((value,index)=>
+		{
+			var ticketUpdate = {"ticket":{"custom_fields":[{"id":360009452511,value:today}]}};
+			
+			client.tickets.update(value.id,ticketUpdate,function(err,req,result){
+			console.log(result);
+			})
+
+		})
+ 
+
+});
+
+
